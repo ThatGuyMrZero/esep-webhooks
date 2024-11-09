83c7b5e5a4cb56b22211937167bd6972639c7fd1
ThatGuyMrZero <147659662+ThatGuyMrZero@users.noreply.github.com>
2024-11-09 17:49:27 -0500

    Add files via upload

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -21,8 +21,7 @@ public class Function
         context.Logger.LogInformation($"FunctionHandler received: {input}");
 
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-        string payload = $"{{\"text\":\"Issue Created: {json.issue.html_url}\"}}";
-
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
         
         var client = new HttpClient();
         var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
